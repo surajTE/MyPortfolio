746c94da9f3ccc1c51636cd3f90e02535decd9b7
surajTE <107943907+surajTE@users.noreply.github.com>
2022-09-03 12:14:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// console.log("Welcome to portfolio.");
+
+// console.log(self);
+// function trans() {
+//     document.querySelector(".s1").innerHTML = "ನಮಸ್ಕಾರ, ನಾನು ಸೂರಜ್";
+//     document.querySelector(".s2").innerHTML = "ಮುಂಭಾಗದ ಡೆವಲಪರ್";
+//     document.querySelector(".s3").innerHTML = "ಲೋರೆಮ್ ಇಪ್ಸಮ್ ಡೋಲರ್ ಸಿಟ್ ಅಮೆಟ್ ಕಾನ್ಸೆಕ್ಟೆಟರ್, ಅಡಿಪಿಸಿಸಿಂಗ್ ಎಲಿಟ್. ಸಿಟ್ ಪ್ರೆಸೆಂಟಿಯಮ್ ಇನ್ಸಿಡುಂಟ್ ಟೆಂಪೊರಾ ನಿಸಿ ಡೊಲೊರೆಮ್ಕ್ ಕಮೊಡಿ?";
+//     document.querySelector(".s4").innerHTML = "ನನ್ನನ್ನು ಸಂಪರ್ಕಿಸಿ";
+//     document.querySelector(".s5").innerHTML = "ಕೆಳಗೆ ಸ್ಕ್ರಾಲ್ ಮಾಡುವುದು";
+// }
