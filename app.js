8ad759ca164d306ff57af5ef9ea796eacc6a4347
surajTE <107943907+surajTE@users.noreply.github.com>
2022-12-21 10:28:21 +0530

    Updated Portfolio

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-// console.log("Welcome to portfolio.");
+const open = document.querySelector('.bar_div');
+		const close = document.querySelector('.close');
+		var tl = gsap.timeline({ defaults: { duration: 1, ease: 'expo.inOut' } });
+		open.addEventListener('click', () => {
+			if (tl.reversed()) {
+				tl.play();
+			} else {
+				tl.to('nav', { right: 0 })
+					.to('nav', { height: '100vh' }, '-=.1')
+					.to('nav ul li a', { opacity: 1, pointerEvents: 'all', stagger: .2 }, '-=.8')
+					.to('.close', { opacity: 1, pointerEvents: 'all' }, "-=.8")
+					.to('nav h2', { opacity: 1 }, '-=1');
+			}
+		});
 
-// console.log(self);
-// function trans() {
-//     document.querySelector(".s1").innerHTML = "ನಮಸ್ಕಾರ, ನಾನು ಸೂರಜ್";
-//     document.querySelector(".s2").innerHTML = "ಮುಂಭಾಗದ ಡೆವಲಪರ್";
-//     document.querySelector(".s3").innerHTML = "ಲೋರೆಮ್ ಇಪ್ಸಮ್ ಡೋಲರ್ ಸಿಟ್ ಅಮೆಟ್ ಕಾನ್ಸೆಕ್ಟೆಟರ್, ಅಡಿಪಿಸಿಸಿಂಗ್ ಎಲಿಟ್. ಸಿಟ್ ಪ್ರೆಸೆಂಟಿಯಮ್ ಇನ್ಸಿಡುಂಟ್ ಟೆಂಪೊರಾ ನಿಸಿ ಡೊಲೊರೆಮ್ಕ್ ಕಮೊಡಿ?";
-//     document.querySelector(".s4").innerHTML = "ನನ್ನನ್ನು ಸಂಪರ್ಕಿಸಿ";
-//     document.querySelector(".s5").innerHTML = "ಕೆಳಗೆ ಸ್ಕ್ರಾಲ್ ಮಾಡುವುದು";
-// }
+		close.addEventListener('click', () => {
+			tl.reverse();
+		});
